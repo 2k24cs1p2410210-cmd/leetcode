d538df65bafb014ac8e09d967e0a583c437ff352
Sushant <2k24.cs1p2410210@gmail.com>
2026-09-26 11:00:31 +0530

    Time: 0 ms (100%), Space: 17.7 MB (26.5%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
-    bool checkBipartite(vector<vector<int>>&graph,int u,vector<int>&color,int curr_color){
-        color[u] = curr_color;
-        for(int &v:graph[u]){
-            if(color[v]==color[u])
-            return false;
-            if(color[v]==-1){
-                int colorOfv = 1-curr_color;
-                if(checkBipartite(graph,v,color,colorOfv)==false)
-                return false;
+    bool checkBipartite(vector<vector<int>>&graph,int node,vector<int>&color,int curr_color){
+        queue<int>q;
+        q.push(node);
+        color[node] = curr_color;
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            for(int &v:graph[u]){
+                if(color[v]==color[u]){
+                    return false;
+                }
+                if(color[v]==-1){
+                    color[v] = 1-color[u];
+                    q.push(v);
+                }
             }
         }
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
         int N = graph.size();
-        // unordered_map<int,vector<int>>adj;
-        // for(auto &edge:graph){
-        //     int a = edge[0];
-        //     int b = edge[1];
-
-        //     adj[a].push_back(b);
-        //     adj[b].push_back(a);
-        // }
         vector<int>color(N,-1);
-        for(int i=0;i<N;i++){    
+        for(int i=0;i<N;i++){
             if(color[i]==-1){
-                if(checkBipartite(graph,i,color,1)==false){
-                    return false;
-                }
+            if(checkBipartite(graph,i,color,1)==false){
+                return false;
+            }
             }
         }
         return true;
